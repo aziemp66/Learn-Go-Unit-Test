30ea39dec8fee5de90b94ec1c2c4dd8241a59fc5
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-18 20:40:26 +0700

    Taking Down Test

--- helper/helloWorld_test.go
+++ helper/helloWorld_test.go
@@ -1,11 +1,25 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func Test_HelloWorld(t *testing.T) {
 	result := HelloWorld("Eko")
 	if result != "Hello Eko!" {
 		//unit test failed
-		panic("Result is not 'Hello Eko!'")
+		// t.Fail()
+		t.Error("Text is Wrong Dumbass, Result Must be 'Hello Eko!'") //t.fail but you can put arguments inside it
 	}
+	fmt.Println("Test_HelloWorld Done")
+}
+
+func Test_HelloWorldAzie(t *testing.T) {
+	result := HelloWorld("Azie")
+	if result != "Hello Azie!" {
+		// t.FailNow()
+		t.Fatal("Text is Wrong you dickhead, Result Must be 'Hello Azie!'") //t.FailNow but you can put arguments inside it
+	}
+	fmt.Println("Test_HelloWorldAzie Done")
 }
