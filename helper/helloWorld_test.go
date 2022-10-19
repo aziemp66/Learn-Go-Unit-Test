fe9231e9a9153cf5efaa794a3e45c05a680490c0
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-19 20:02:39 +0700

    Assertions And Require

--- helper/helloWorld_test.go
+++ helper/helloWorld_test.go
@@ -3,6 +3,9 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	assert "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_HelloWorld(t *testing.T) {
@@ -13,6 +16,7 @@ func Test_HelloWorld(t *testing.T) {
 		t.Error("Text is Wrong Dumbass, Result Must be 'Hello Eko!'") //t.fail but you can put arguments inside it
 	}
 	fmt.Println("Test_HelloWorld Done")
+
 }
 
 func Test_HelloWorldAzie(t *testing.T) {
@@ -22,4 +26,16 @@ func Test_HelloWorldAzie(t *testing.T) {
 		t.Fatal("Text is Wrong you dickhead, Result Must be 'Hello Azie!'") //t.FailNow but you can put arguments inside it
 	}
 	fmt.Println("Test_HelloWorldAzie Done")
+}
+
+func Test_HelloWorldAssertion(t *testing.T) {
+	result := HelloWorld("Azie")
+	assert.Equal(t, "Hello Azie!", result, "Result Must be 'Hello Azie!'")
+	fmt.Println("Dieksekusi")
+}
+
+func Test_HelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Melza")
+	require.Equal(t, "Hello Melza!", result, "Result Must Be 'Hello Melza!'")
+	fmt.Println("Tidak Dieksekusi")
 }
