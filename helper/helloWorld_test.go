a2da49b8cbea622c637670521ede54681bb55760
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-20 10:51:59 +0700

    Sub Test & Table Test

--- helper/helloWorld_test.go
+++ helper/helloWorld_test.go
@@ -2,10 +2,11 @@ package helper
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"runtime"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
@@ -19,19 +20,22 @@ func TestMain(m *testing.M) {
 func Test_HelloWorld(t *testing.T) {
 	result := HelloWorld("Eko")
 	if result != "Hello Eko!" {
-		//unit test failed
+		// unit test failed
 		// t.Fail()
-		t.Error("Text is Wrong Dumbass, Result Must be 'Hello Eko!'") //t.fail but you can put arguments inside it
+		t.Error(
+			"Text is Wrong Dumbass, Result Must be 'Hello Eko!'",
+		) // t.fail but you can put arguments inside it
 	}
 	fmt.Println("Test_HelloWorld Done")
-
 }
 
 func Test_HelloWorldAzie(t *testing.T) {
 	result := HelloWorld("Azie")
 	if result != "Hello Azie!" {
 		// t.FailNow()
-		t.Fatal("Text is Wrong you dickhead, Result Must be 'Hello Azie!'") //t.FailNow but you can put arguments inside it
+		t.Fatal(
+			"Text is Wrong you dickhead, Result Must be 'Hello Azie!'",
+		) // t.FailNow but you can put arguments inside it
 	}
 	fmt.Println("Test_HelloWorldAzie Done")
 }
@@ -46,7 +50,6 @@ func Test_HelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Melza")
 	require.Equal(t, "Hello Melza!", result, "Result Must Be 'Hello Melza!'")
 	fmt.Println("Tidak Dieksekusi")
-
 }
 
 func Test_Skip(t *testing.T) {
@@ -58,4 +61,41 @@ func Test_Skip(t *testing.T) {
 
 	result := HelloWorld("Eko")
 	require.Equal(t, "Hello Eko!", result)
+}
+
+func Test_SubTest(t *testing.T) {
+	t.Run("Melza", func(t *testing.T) {
+		result := HelloWorld("Melza")
+		require.Equal(t, "Hello Melza!", result)
+	})
+	t.Run("Pratama", func(t *testing.T) {
+		result := HelloWorld("Pratama")
+		require.Equal(t, "Hello Pratama!", result)
+	})
+}
+
+func Test_HelloWorldTable(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Eko",
+			request:  "Eko",
+			expected: "Hello Eko!",
+		},
+		{
+			name:     "Melza",
+			request:  "Melza",
+			expected: "Hello Melza!",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			assert.Equal(t, test.expected, result)
+		})
+	}
 }
